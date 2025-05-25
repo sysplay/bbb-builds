b1208cb76d665f27bd1f2f75539a357719c7893b
Anil Kumar Pugalia <anil@sysplay.in>
2025-05-25 21:47:33 +0530

    Adding the first-cut custom layer

--- Yocto/setup.sh
+++ Yocto/setup.sh
@@ -6,5 +6,11 @@ sudo urpmi chrpath diffstat lz4 rpcgen
 # Get the yocto sources
 git clone -b kirkstone https://git.yoctoproject.org/git/poky
 
+# Setup a custom-layer
+bitbake-layers create-layer meta-sysplay
+
 # Initialize the build environment - this creates & takes into the build directory
 . poky/oe-init-build-env
+
+# Add the custom layer to it
+bitbake-layers add-layer ../meta-sysplay
